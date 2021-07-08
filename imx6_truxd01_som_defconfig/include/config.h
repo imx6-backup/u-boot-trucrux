48783a554d2b2912f6c8d89185e37266bde4ab13
Nikhil Anjane <nikhil.anjane@trunexa.com>
2021-07-08 13:02:48 +0530

    Nilima: sdcard SOM changes

---
+++ imx6_truxd01_som_defconfig/include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/imx6_truxd01_som/imx6_truxd01.cfg
+#define CONFIG_BOARDDIR board/freescale/imx6_truxd01_som
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/imx6_truxd01_som.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ imx6_truxd01_som_defconfig/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Jun 01 2021"
+#define U_BOOT_TIME "17:11:55"
+#define U_BOOT_TZ "+0530"
+#define U_BOOT_DMI_DATE "06/01/2021"
